a6f2f4f32bbd236f50cd508aa4f32d411d3a65d5
DrColling <lincoln@colling.net.nz>
2018-02-05 13:34:51 +0000

    added code for installing ptb

---
+++ InstallPTB.m
@@ -0,0 +1,5 @@
+unzip('https://git.io/vNC8q')
+movefile Psychtoolbox-3-PTB_Beta-2017-10-08_V3.0.14/Psychtoolbox Psychtoolbox
+rmdir('Psychtoolbox-3-PTB_Beta-2017-10-08_V3.0.14','s')
+cd Psychtoolbox
+SetupPsychtoolbox

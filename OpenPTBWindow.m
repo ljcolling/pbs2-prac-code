53700a15008b4ba6c6128cc1e1a5f281c9482a9a
DrColling <lincoln@colling.net.nz>
2018-02-16 11:11:49 +0000

    Updated OpenPTBWindow.m
    Provided two options to either open a full screen window or a small window

--- OpenPTBWindow.m
+++ OpenPTBWindow.m
@@ -1,4 +1,11 @@
 function [w,rect] = OpenPTBWindow()
 
 Screen('Preference', 'SkipSyncTests', 1)
+
+% uncomment the appropriate line below
+% the first line opens a small window that is 800 x 600 pixels
+% the second line opens a full screen window
+% the first line is uncommented by default, so change it once everything is working!
+
 [w,rect] = PsychImaging('OpenWindow',0,[],[0 0 800 600]);
+% [w,rect] = PsychImaging('OpenWindow',0,[]);

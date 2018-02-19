2ed5c60d56f9334d486bf83a3426b2dd81da51df
DrColling <lincoln@colling.net.nz>
2018-02-19 18:29:27 +0000

    updated update

--- getfiles.m
+++ getfiles.m
@@ -1,17 +1,6 @@
 disp('Now I will download some files')
 websave('update.m','https://github.com/ljcolling/pbs2-prac-code/raw/master/update.m')
 
-try
-    update
-catch
-end
+update
 
-try
-    path(path,sprintf('%%s',load('newpath.ini','-ascii')))
-catch
-end
 
-try
-    delete MyPTBTask.m
-catch
-end
